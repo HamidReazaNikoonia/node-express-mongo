e3cbff4665cd21522883c4f6fe7dcbb742663f84
HamidReazaNikoonia <hamidrezanik00nia@gmail.com>
2020-03-28 02:27:30 +0430

    implement env variable validation with joi

--- src/config/vars.js
+++ src/config/vars.js
@@ -1,10 +1,38 @@
+/* eslint-disable no-unused-vars */
 const path = require('path');
+const joi = require('joi');
 
 // import .env variables
 require('dotenv-safe').load({
   path: path.join(__dirname, '../../.env'),
   sample: path.join(__dirname, '../../.env.example'),
 });
+
+const envVarsSchema = joi.object({
+  NODE_ENV: joi.string()
+    .allow(['development', 'dev', 'production', 'test', 'provision', 'combined'])
+    .required(),
+  PORT: joi.number()
+    .required(),
+  LOGGER_LEVEL: joi.string()
+    .allow(['error', 'warn', 'info', 'verbose', 'debug', 'silly'])
+    .default('info'),
+  LOGGER_ENABLED: joi.boolean()
+    .truthy('TRUE')
+    .truthy('true')
+    .falsy('FALSE')
+    .falsy('false')
+    .default(true),
+}).unknown()
+  .required();
+
+const {
+  error,
+  value: envVars,
+} = joi.validate(process.env, envVarsSchema);
+if (error) {
+  throw new Error(`Config validation error: ${error.message}`);
+}
 
 module.exports = {
   env: process.env.NODE_ENV,
